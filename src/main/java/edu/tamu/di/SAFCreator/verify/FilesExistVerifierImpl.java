d0f6b31328a4480ed02e91dc7eff4f7c3d12763c
Kevin Day <kday@library.tamu.edu>
2018-01-12 10:21:16 -0600

    Bugfix: URL redirects from one protocol to another are not redirecting
    The java classes do not support automatically handling redirects from one protocol to another.
    Manually create a custom URL redirection with basic loop detection and a hard limit 20 max redirects.
    
    The validator is being used to update the bitstream source URI with the resulting redirect.

--- src/main/java/edu/tamu/di/SAFCreator/verify/FilesExistVerifierImpl.java
+++ src/main/java/edu/tamu/di/SAFCreator/verify/FilesExistVerifierImpl.java
@@ -5,8 +5,11 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.net.URLDecoder;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -20,6 +23,7 @@ import edu.tamu.di.SAFCreator.model.Item;
 import edu.tamu.di.SAFCreator.model.Verifier;
 
 public class FilesExistVerifierImpl implements Verifier {
+	private static int MaxRedirects = 20;
 
 	public List<Problem> verify(Batch batch) 
 	{
@@ -84,11 +88,61 @@ public class FilesExistVerifierImpl implements Verifier {
 
 									conn = (HttpURLConnection) source.toURL().openConnection();
 									conn.setRequestMethod("HEAD");
+									conn.setInstanceFollowRedirects(true);
 									conn.getInputStream().close();
 
 									int response = conn.getResponseCode();
 
-									if (response != 200) {
+									if (response == HttpURLConnection.HTTP_MOVED_TEMP || response == HttpURLConnection.HTTP_MOVED_PERM || response == HttpURLConnection.HTTP_SEE_OTHER) {
+										int totalRedirects = 0;
+										HashSet<String> previousUrls = new HashSet<String>();
+										previousUrls.add(source.toString());
+
+										do {
+											if (totalRedirects++ > MaxRedirects) {
+												Problem missingFile = new Problem(bitstream.getRow(), bitstream.getColumn(), generatesError(), "Unable to validate file URL " + source.toString() + ", too many redirects.");
+												missingFiles.add(missingFile);
+												response = HttpURLConnection.HTTP_OK;
+												break;
+											}
+
+											//String redirectCookies = conn.getHeaderField("Set-Cookie");
+											String redirectTo = conn.getHeaderField("Location");
+											URL redirectToUrl = new URL(redirectTo);
+											if (previousUrls.contains(redirectTo)) {
+												Problem missingFile = new Problem(bitstream.getRow(), bitstream.getColumn(), generatesError(), "Unable to validate file URL " + source.toString() + ", recursive redirection.");
+												missingFiles.add(missingFile);
+												response = HttpURLConnection.HTTP_OK;
+												break;
+											}
+											previousUrls.add(redirectTo);
+
+											String schema = redirectToUrl.getProtocol();
+											if (schema.equalsIgnoreCase("http") || schema.equalsIgnoreCase("https") || schema.equalsIgnoreCase("ftp")) {
+												conn.disconnect();
+												conn = (HttpURLConnection) redirectToUrl.openConnection();
+												conn.setRequestMethod("HEAD");
+												conn.setInstanceFollowRedirects(true);
+												//conn.setRequestProperty("Cookie", redirectCookies);
+												conn.getInputStream().close();
+
+												response = conn.getResponseCode();
+												if (response == HttpURLConnection.HTTP_OK) {
+													String redirectFrom = source.toURL().toString();
+													Problem warning = new Problem(bitstream.getRow(), bitstream.getColumn(), false, "Redirecting " + redirectFrom + " to " + redirectToUrl.toString() + ".");
+													missingFiles.add(warning);
+													bitstream.setSource(redirectToUrl.toURI());
+												}
+											}
+											else {
+												String redirectFrom = source.toURL().toString();
+												Problem missingFile = new Problem(bitstream.getRow(), bitstream.getColumn(), generatesError(), "Refusing to redirect from " + redirectFrom + " to " + redirectToUrl.toString() + " due to protocol.");
+												missingFiles.add(missingFile);
+											}
+										} while (response == HttpURLConnection.HTTP_MOVED_TEMP || response == HttpURLConnection.HTTP_MOVED_PERM || response == HttpURLConnection.HTTP_SEE_OTHER);
+									}
+
+									if (response != HttpURLConnection.HTTP_OK) {
 										Problem missingFile = new Problem(bitstream.getRow(), bitstream.getColumn(), generatesError(), "Unable to validate file URL " + source.toString() + ", HTTP Response Code: " + response + ".");
 										missingFiles.add(missingFile);
 									}
@@ -99,6 +153,9 @@ public class FilesExistVerifierImpl implements Verifier {
 									Problem missingFile = new Problem(bitstream.getRow(), bitstream.getColumn(), generatesError(), "Source file URL " + source.toString() + " had a connection error, message: " + e.getMessage() + ".");
 									missingFiles.add(missingFile);
 								} catch (InterruptedException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								} catch (URISyntaxException e) {
 									// TODO Auto-generated catch block
 									e.printStackTrace();
 								} finally {
